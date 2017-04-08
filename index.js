8b9b2ebaeaf2de063ccb94f8b1a706cda9ba86c1
Dima Kovalevskyi <dimakovalevskyi@gmail.com>
2017-04-08 13:01:07 +0300

    set jsonp as response

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var app = express();
 
 app.get('/', function (req, res) {
     var time = new Date();
-
-    res.json(time);
+    res.jsonp(time);
 });
-
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
